7d01851b6b0a37c89cba8247307cb63d96626716
Robert Vollmert <rob@vllmrt.net>
2018-01-20 00:22:04 +0100

    tests

---
+++ tests/Example.elm
@@ -0,0 +1,26 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Parse
+import Parser
+import Test exposing (..)
+import Types
+
+
+suite : Test
+suite =
+    describe "module Parsers"
+        [ describe "basic parsers"
+            [ test "parses a known integer" <|
+                \_ ->
+                    Expect.equal
+                        (Ok (Types.Int 42))
+                        (Parser.run Parse.integer "42")
+            , fuzz int "parses a random integer" <|
+                \i ->
+                    Expect.equal
+                        (Ok (Types.Int i))
+                        (Parser.run Parse.integer (toString i))
+            ]
+        ]
