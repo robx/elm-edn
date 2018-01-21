64c3c7ac130576e408e041718d61d3b82b4929e2
Robert Vollmert <rob@vllmrt.net>
2018-01-21 01:59:53 +0100

    more test reformatting

--- tests/Example.elm
+++ tests/Example.elm
@@ -67,112 +67,61 @@ suite : Test
 suite =
     describe "module Parsers"
         [ nestedList
-        , describe "basic parsers"
-            [ test "parses a known integer" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (Int 42))
-                        (Parser.run Parse.value "42")
-            , fuzz int "parses a random integer" <|
-                \i ->
-                    Expect.equal
-                        (Ok (Int i))
-                        (Parser.run Parse.value (toString i))
-            , test "parse a known string" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (String "a string\twith\\escape\"'s"))
-                        (Parser.run Parse.value "\"a string\\twith\\\\escape\\\"'s\"")
-            , test "parse a known bool" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (Bool True))
-                        (Parser.run Parse.value "true")
-            , test "parse an empty list" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List []))
-                        (Parser.run Parse.value "()")
-            , test "parse another empty list" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List []))
-                        (Parser.run Parse.value "( ,, )")
-            , test "parse a one-element list" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil ]))
-                        (Parser.run Parse.value "(nil)")
-            , test "parse a one-element list with leading space" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil ]))
-                        (Parser.run Parse.value "( nil)")
-            , test "parse a one-element list with trailing space" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil ]))
-                        (Parser.run Parse.value "(nil )")
-            , test "parse a one-element list with spaces" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil ]))
-                        (Parser.run Parse.value "( nil )")
-            , test "parse a straight list of nils" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil, Nil, Nil, Nil ]))
-                        (Parser.run Parse.value "(nil nil nil nil)")
-            , test "parse a spaced list of nils" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Nil, Nil, Nil, Nil ]))
-                        (Parser.run Parse.value "( , nil,nil   nil,, nil )")
-            , test "parse a simple list" <|
-                \_ ->
-                    Expect.equal
-                        (Ok (List [ Bool False, String "hello, world", Int -15 ]))
-                        (Parser.run Parse.value """(false "hello, world",  -15,)""")
-            , test "nesting things" <|
-                \_ ->
-                    Expect.equal
-                        (Ok
-                            (Vector
-                                [ List [ Int 1, String "yo" ]
-                                , Nil
-                                , Map [ ( List [ Bool True ], String "who" ), ( Nil, Int -3 ) ]
-                                ]
-                            )
-                        )
-                        (Parser.run Parse.value """[ (1, "yo"), nil, {(true) "who", nil -3} ]""")
-            , skip <|
-                test "list of empty maps" <|
-                    \_ ->
-                        Expect.equal
-                            (Ok (List [ Map [], Map [], Map [] ]))
-                            (Parser.run Parse.value "({}{}{})")
-            , skip <|
-                test "triples event" <|
-                    \_ ->
-                        Expect.equal
-                            (Ok
-                                (Map
-                                    [ ( Symbol "cols", Int 4 )
-                                    , ( Symbol "rows", Int 3 )
-                                    , ( Symbol "deckSize", Int 0 )
-                                    , ( Symbol "cards", Map [] )
-                                    , ( Symbol "scores"
-                                      , Map
-                                            [ ( Symbol "match", Int 0 )
-                                            , ( Symbol "matchWrong", Int 0 )
-                                            , ( Symbol "noMatch", Int 0 )
-                                            , ( Symbol "noMatchWrong", Int 0 )
-                                            ]
-                                      )
+        , test "basic parsers" <|
+            \_ ->
+                parse
+                    [ "42" => Int 42
+                    , "\"a string\\twith\\\\escape\\\"'s\""
+                        => String "a string\twith\\escape\"'s"
+                    , "true" => Bool True
+                    , "()" => List []
+                    , "( ,, )" => List []
+                    , "(nil)" => List [ Nil ]
+                    , "( nil)" => List [ Nil ]
+                    , "(nil )" => List [ Nil ]
+                    , "( nil )" => List [ Nil ]
+                    , "(nil nil nil nil)" => List [ Nil, Nil, Nil, Nil ]
+                    , "( , nil,nil   nil,, nil )" => List [ Nil, Nil, Nil, Nil ]
+                    , """(false "hello, world",  -15,)"""
+                        => List [ Bool False, String "hello, world", Int -15 ]
+                    ]
+                    Parse.value
+        , fuzz int "parses a random integer" <|
+            \i ->
+                Expect.equal
+                    (Ok (Int i))
+                    (Parser.run Parse.value (toString i))
+        , test "nesting things" <|
+            \_ ->
+                parse
+                    [ """[ (1, "yo"), nil, {(true) "who", nil -3} ]"""
+                        => Vector
+                            [ List [ Int 1, String "yo" ]
+                            , Nil
+                            , Map [ ( List [ Bool True ], String "who" ), ( Nil, Int -3 ) ]
+                            ]
+                    , "({}{}{})" => List [ Map [], Map [], Map [] ]
+                    ]
+                    Parse.value
+        , test "triples" <|
+            \_ ->
+                parse
+                    [ """{:cols 4 :rows 3 :matchSize 3 :deckSize 0 :cards{}:scores{"Rob"{:match 0 :matchWrong 0 :noMatch 0 :noMatchWrong 0}}}
+"""
+                        => Map
+                            [ ( Symbol "cols", Int 4 )
+                            , ( Symbol "rows", Int 3 )
+                            , ( Symbol "deckSize", Int 0 )
+                            , ( Symbol "cards", Map [] )
+                            , ( Symbol "scores"
+                              , Map
+                                    [ ( Symbol "match", Int 0 )
+                                    , ( Symbol "matchWrong", Int 0 )
+                                    , ( Symbol "noMatch", Int 0 )
+                                    , ( Symbol "noMatchWrong", Int 0 )
                                     ]
-                                )
-                            )
-                            (Parser.run Parse.value """{:cols 4 :rows 3 :matchSize 3 :deckSize 0 :cards{}:scores{"Rob"{:match 0 :matchWrong 0 :noMatch 0 :noMatchWrong 0}}}
-""")
-            ]
+                              )
+                            ]
+                    ]
+                    Parse.value
         ]
